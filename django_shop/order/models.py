d621441648579295a62f328dadb9ade32a1f7e92
YoungJun <coke05288@gmail.com>
2023-06-16 23:51:08 +0900

    230616 | set models

--- django_shop/order/models.py
+++ django_shop/order/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Order(models.Model):
+    # on_delete : 연결된 외래키 아이템이 삭제될 때 해당 Order 아이템을 어떻게 지워줄지
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE, verbose_name="사용자")
+    product = models.ForeignKey('product.Product', on_delete=models.CASCADE, verbose_name="제품")
+    quantity = models.IntegerField(verbose_name="수량")
+    register_date = models.DateTimeField(auto_now_add=True, verbose_name="주문날짜")
+
+    class Meta:
+        db_table = 'shoppingmall_order'
+        verbose_name = '주문'
+        verbose_name_plural = '주문'
+
--- django_shop/user/models.py
+++ django_shop/user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class user(models.Model):
+    email = models.EmailField(verbose_name="이메일")
+    password = models.CharField(max_length=64, verbose_name="비밀번호")
+    register = models.DateTimeField(auto_now_add=True, verbose_name="가입일")
+    
+    class Meta:
+        db_table = 'shoppingmall_user'
+        verbose_name = '사용자'
+        verbose_name_plural = '사용자'
