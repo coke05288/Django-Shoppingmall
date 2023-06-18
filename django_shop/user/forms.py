cc0cc07fe98bd4005372a17bfdb4475d0f9a75da
YoungJun <coke05288@gmail.com>
2023-06-19 00:09:07 +0900

    230619 | feature user register & Login

--- django_shop/user/forms.py
+++ django_shop/user/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.contrib.auth.hashers import check_password, make_password
 from .models import User
 
-class RegisterFrom(forms.Form):
+class RegisterForm(forms.Form):
     email = forms.EmailField(
         error_messages={
             'required' : '이메일을 작성해주세요.'
@@ -37,6 +38,41 @@ class RegisterFrom(forms.Form):
             else:
                 temp_user = User(
                     email = c_email,
-                    password = c_password
+                    password = make_password(c_password)
                 )
                 temp_user.save()
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        error_messages={
+            'required' : '이메일을 작성해주세요.'
+        },
+        max_length=256,
+        label='이메일',
+    )
+    password = forms.CharField(
+        error_messages={
+            'required' : '비일번호를 입력해주세요.'
+        },
+        widget=forms.PasswordInput,
+        label='비밀번호',
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        c_email = cleaned_data.get('email')
+        c_password = cleaned_data.get('password')
+
+
+        if c_email and c_password:
+            try:
+                user = User.objects.get(email=c_email)
+                print(user.email, user.password)
+            except User.DoesNotExist:
+                self.add_error('email', '아이디가 없습니다.')
+                return
+            
+            if not check_password(c_password, user.password):
+                self.add_error('password', '비밀번호가 틀렸습니다.')
+            else:
+                self.email = user.email
